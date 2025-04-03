daa091836e37a8c87fc0929a7b316edfae484180
Welberth <welbertholiveira250@gmail.com>
2025-04-03 15:22:27 -0300

    Ajuste do z-index dinâmico

--- Scripts/arvores.gd
+++ Scripts/arvores.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
-@export var player: CharacterBody2D
+@onready var player = get_node("/Mundo/player")  # Ajuste o caminho correto do Player
 
 func _ready() -> void:
 	pass
 
-func _process(_delta: float) -> void:
-	pass
-	
+func _process(delta):
+	if player:
+		if player.position.y > position.y:
+			z_index = 1  # Player atrás da árvore
+		else:
+			z_index = 2  # Player na frente da árvore
